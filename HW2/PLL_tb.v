56bdf14532f09ce5a333ea378d55e418b8aa08bf
yirousong <127753598+yirousong@users.noreply.github.com>
2026-02-19 20:31:15 +0800

    Update PLL_tb.v

--- HW2/PLL_tb.v
+++ HW2/PLL_tb.v
@@ -1,43 +1,37 @@
-`timescale 1ns/100ps
+`timescale 100ps/1ps
 module PLL_tb();
 
-    parameter period = 10;
-    reg clk_ref;
-    reg rst_n;
-    reg [3:0] N;
-    reg enable;
-    wire clk_DCO;
-    wire lock;
-    wire clk_div;
-    wire lead_lag;
-    // wire [1:0] alpha;
+    parameter period = 100;
+
+    reg        clk_ref;
+    reg        rst_n;
+    reg        enable;
+    reg  [3:0] N;
+
+    wire       clk_out;
+    // wire       clk_div;
+    wire       lead_lag;
     wire [7:0] lambda;
 
-    PLL UUT(.clk_ref(clk_ref),
+    PLL UUT (
+            .clk_ref(clk_ref),
             .rst_n(rst_n),
             .N(N),
             .enable(enable),
-            .clk_DCO(clk_DCO),
-            .lock(lock)
-            .clk_div(clk_div)
-            .lead_lag(lead_lag)
-            .alpha(alpha)
-            .lambda(lambda));
+            .clk_out(clk_out),
+            // .clk_div(clk_div),
+            .lead_lag(lead_lag),
+            .lambda(lambda)
+            );
 
     always #(period/2) clk_ref = ~clk_ref;
 
     initial begin
-      clk_ref= 1;
-      N      = 10;
-      enable = 0;
-      rst_n  = 1;
-      #(period)
-      rst_n  = 0;
-      #(period * 10)
-      rst_n  = 1;
-      #(period * 5)
-      enable = 1;
-      #1000000
+      clk_ref  = 0; rst_n  = 1; N = 10; enable = 0;
+      #(period * 2) rst_n  = 0;
+      #(period * 2) rst_n  = 1;
+      #(period * 2) enable = 1;
+      #100000
       $finish;
     end
 
